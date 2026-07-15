18ab91da3200c8dbb8b231656df344db01a583a5
András Farkas <r4verino@gmail.com>
2026-07-15 11:35:04 +0200

    Initial commit: E.ON W1000 HA integration skeleton

---
+++ custom_components/eon_w1000/const.py
@@ -0,0 +1,33 @@
+"""Constants for E.ON W1000 integration."""
+
+DOMAIN = "eon_w1000"
+PLATFORMS = ["sensor"]
+
+# Config entry keys
+CONF_IMAP_HOST = "imap_host"
+CONF_IMAP_PORT = "imap_port"
+CONF_IMAP_USER = "imap_user"
+CONF_IMAP_PASS = "imap_pass"
+CONF_POLL_INTERVAL = "poll_interval"
+CONF_EMAIL_SENDER = "email_sender"
+CONF_EMAIL_SUBJECT = "email_subject"
+CONF_DAYS_BACK = "days_back"
+
+# Defaults
+DEFAULT_IMAP_PORT = 993
+DEFAULT_POLL_INTERVAL = 60  # minutes
+DEFAULT_EMAIL_SENDER = "noreply@eon.com"
+DEFAULT_EMAIL_SUBJECT = "[EON-W1000]"
+DEFAULT_DAYS_BACK = 7
+
+# Sensor IDs
+SENSOR_GRID_IMPORT = "grid_import"
+SENSOR_GRID_EXPORT = "grid_export"
+
+# Statistics
+STATISTIC_IMPORT_ID = "eon_w1000:grid_import"
+STATISTIC_EXPORT_ID = "eon_w1000:grid_export"
+
+# Storage keys
+STORAGE_VERSION = 1
+STORAGE_KEY = "eon_w1000_processed"
